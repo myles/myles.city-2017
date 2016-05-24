c7f877322a06268b032765355c7b98b318350e68
Myles Braithwaite <me@mylesbraithwaite.com>
2016-05-24 17:24:21 -0400

    Starting from scratch.

--- grunt/phplint.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (grunt) {
-    'use strict';
-
-    var config = {
-        files: ['./site/*.php']
-    };
-
-    grunt.config.set('phplint', config);
-};
--- site/assets/javascript/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('.page-title h1').lettering();
-    $('.page-title h1').fitText(0.6);
-	$('.site-title h2').fitText(1.4);
-    
-	SVGInjector($('.page-title img'), {
-	    pngFallback: '/assets/images/'
-	});
-});
